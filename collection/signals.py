0493d7ace9beac18140477c409cb354b8d965cf4
eknathyadav <yadaveknath98@gmail.com>
2023-01-30 00:50:23 +0530

    removed unnecessary files

--- collection/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import User
-from rest_framework_simplejwt.tokens import RefreshToken
-
-
-@receiver(post_save, sender=User)
-def generate_token(sender, user_instance, **kwargs):
-    RefreshToken.for_user(user_instance)
